a8e9b779dc9433d2c893bad01b626389555ec27d
serra <serra@modemgirl.ca>
2022-03-17 17:51:23 -0700

    add counter display

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 
   $("#twiit-text").keydown(function(){
-    $("#counter").text(this.value.length+1);
+    let count = this.value.length + 1;
+
+    if (count > 140){
+      count -= 140;
+      count = (0 - count);
+      $("#counter").text(count);
+      //add css class to change colour
+    } else {
+      $("#counter").text(this.value.length + 1);
+    }
+        
   });
 
 });
